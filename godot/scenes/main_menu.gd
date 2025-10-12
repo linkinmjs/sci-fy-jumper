fbaa3ed28f93480db210a12bb96f5fccbe51b2df
unknown <linkinmjs@gmail.com>
2025-10-12 03:40:44 -0300

    Add backgrounds

---
+++ godot/scenes/main_menu.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	animation_player.play("idle")
+
+func _process(delta: float) -> void:
+	pass
