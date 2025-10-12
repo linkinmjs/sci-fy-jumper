5c674f286f3e0ecde76cb293fdf031277a8e627c
unknown <linkinmjs@gmail.com>
2025-10-12 13:15:54 -0300

    Add main menu and pause

---
+++ godot/scenes/UI/pause_menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var pause_menu: Panel = $CanvasLayer/Panel
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("esc"):
+		pause_or_unpause()
+
+func pause_or_unpause() -> void:
+	if get_tree().paused == true:
+		pause_menu.hide()
+		get_tree().paused = false
+	elif get_tree().paused == false:
+		pause_menu.show()
+		get_tree().paused = true
--- godot/scenes/main_menu.gd
+++ godot/scenes/main_menu.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+const GAME_SCENE: PackedScene = preload("res://scenes/game.tscn")
 
 func _ready() -> void:
 	animation_player.play("idle")
 
-func _process(delta: float) -> void:
-	pass
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot"):
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
