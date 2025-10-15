89752e5997b8ade5a273029ed635bec8984a0964
unknown <linkinmjs@gmail.com>
2025-10-15 13:09:41 -0300

    Fix level 1 and add quit game on main menu

--- godot/scenes/main_menu.gd
+++ godot/scenes/main_menu.gd
@@ -17,6 +17,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		animation_player.play("start")
 		await main_menu_music.finished
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
-
+	elif event.is_action_pressed("esc") and !starting:
+		get_tree().quit()
+		
 func _on_main_menu_music_finish() -> void:
 	main_menu_music.play(0.0)
